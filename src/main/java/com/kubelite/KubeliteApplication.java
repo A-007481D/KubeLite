a5fb9be9338f53d0600721f33d96da1b95f81039
A-007481D <labidabdelmalek@gmail.com>
2025-12-13 18:05:16 +0100

    initial project structure and pom deps

---
+++ src/main/java/com/kubelite/KubeliteApplication.java
@@ -0,0 +1,13 @@
+package com.kubelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubelite/KubeliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubelite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
